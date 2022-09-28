53442a93b5e4199b8c2225194440763291afb1bf
Teddy Graveleau <tgraveleau@noelse.com>
2022-09-28 10:26:23 +0200

    🎨 Improve layout structure

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -4,16 +4,25 @@ import { Header } from './Header'
 import { MainContainer } from './MainContainer'
 import { Footer } from './Footer';
 import { Sidenav } from './Sidenav';
+import './Layout.css'
 
 export const Layout: React.FC = () => {
   return (
-    <div style={{height: '70vh', marginTop: '15vh'}}>
-        <Header/>
-        <MainContainer>
-            <Outlet/>
-        </MainContainer>
-        <Sidenav/>
-        <Footer/>
+    <div className='Layout'>
+        <div className='Layout_header'>
+            <Header/>
+        </div>
+        <div className='Layout_main-container'>
+            <MainContainer>
+                <Outlet/>
+            </MainContainer>
+        </div>
+        <div className='Layout_sidenav'>
+            <Sidenav/>
+        </div>
+        <div className='Layout_footer'>
+            <Footer/>
+        </div>
     </div>
   )
 }
